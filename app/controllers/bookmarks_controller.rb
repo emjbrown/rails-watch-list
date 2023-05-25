81ed0e8cff04c260e264eef961a976d19b754927
Emily Brown <emi.jbrown@gmail.com>
2023-05-25 16:17:43 -0400

    created bookmarks form

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,30 @@
+class BookmarksController < ApplicationController
+  # def index
+  #   @bookmarks = Bookmark.all
+  # end
+  # is it because I need a /bookmarks path in order
+  # to create bookmarks/new?
+
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.save
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(:id)
+    @bookmark.destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
