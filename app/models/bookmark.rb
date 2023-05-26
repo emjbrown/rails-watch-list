c48bbb54bc62687100c9cbe6b6a70885b2e356a7
Emily Brown <emi.jbrown@gmail.com>
2023-05-26 16:31:59 -0400

    added activestorage for cloudinary

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie, dependent: :destroy
   belongs_to :list
+  has_one_attached :photo
 
   validates_associated :movie, :list
   validates :movie_id, uniqueness: { scope: :list_id }
